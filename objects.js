eb17598aea0962bc23e9ecd319470b22d4a7745a
Alexa Schaeffer <alps231@gmail.com>
2018-08-26 20:36:15 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,17 @@
+var playlist = {
+  "Jessie Reyez": "Sola",
+  Kehlani: "Honey"
+}
+
+function updatePlaylist(playlist, artist, song){
+  playlist = [artist]: key
+  return playlist
+}
+
+const playlist = {Ella: "Boo"}
+
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist]
+  return playlist
+}
